cf4403dc7e6f46e0f60958c8a8fedec3d5d8f3de
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-09-16 11:57:44 -1000

    Just added in all the posts and gets for the datatables, my next step will be to work on /api/users/:id and then doing put for /api/topics:id

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 
 const User = require('./models').User;
+const Topic = require('./models').Topic;
+const Message = require('./models').Message;
 
 const app = express();
 const db = require('./models');
@@ -17,12 +19,11 @@ app.get('*', (req, res) => {
     });
 });
 
-app.route('/user')
+app.route('/api/users')
     .get((req, res) => {
         User.findAll()
             .then((users) => {
-                res.json(users);
-                console.log('yippie')
+                res.json(users); 
             })
             .catch((err) => {
                 console.log(err);
@@ -34,18 +35,64 @@ app.route('/user')
                 name: req.body.name
             })
                 .then((users) => {
-                    console.log('we posted')
+                    console.log('we posted name')
                     res.json(users);
                 })
             .catch((err) => {
                 console.log(err)
+            });
+    });
+
+
+    app.route('/api/topic')
+    .get((req, res) => {
+        Topic.findAll()
+            .then((topics) => {
+                res.json(topics);   
             })
+            .catch((err) => {
+                console.log(err);
+            });
     })
+    .post((req, res) => {
+        console.log('posting', req.body)
+        Topic.create({
+                name: req.body.name
+            })
+                .then((topics) => {
+                    console.log('we posted topic')
+                    res.json(topics);
+                })
+            .catch((err) => {
+                console.log(err)
+            });
+    });
 
 
-
-
-
+    app.route('/api/message')
+    .get((req, res) => {
+        Message.findAll()
+            .then((messages) => {
+                res.json(messages);
+                
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    })
+    .post((req, res) => {
+        console.log('posting', req.body)
+        Message.create({
+                body: req.body.body
+            })
+                .then((messages) => {
+                    console.log('we posted messages')
+                    res.json(messages);
+                })
+            .catch((err) => {
+                console.log(err)
+            });
+    });
 
 
 
